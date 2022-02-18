e60a9b12705044653d6987ea532489fb1eca41d5
Jahleels <jahleels20@gmail.com>
2022-02-18 14:45:39 -0500

    Fixing dataTables bugs

--- controllers/personaController.js
+++ controllers/personaController.js
@@ -5,27 +5,36 @@ module.exports = {
         try {
             const listPersonas = await models.Persona.find({});
             res.status(200).json(listPersonas); 
+            console.log(listPersonas)
         } catch( err ) {
             res.status(500).json({msg: `Ocurrió un error ${err}`});
         }
     },
 
     postPersona: async (req, res) => {
+        console.log(req.body)
         try {
-            await models.Persona.create({
-                name: req.body.name,
-                cc: req.body.cc,
-                phoneNumber: req.body.phoneNumber,
-                email: req.body.email,
-                city: req.body.city,
-                address: req.body.address,
-                barrio: req.body.barrio,
-                puesto: req.body.puesto,
-                mesa: req.body.mesa,
-                lider: req.body.lider,
-                user: req.body.user
-            });
-            res.status(200).json({msg: "Persona guardada"});
+            let findOne = await models.Persona.findOne({cc:req.body.cc});
+            console.log(this.findOne)
+            if(findOne) {
+                res.status(418).send({msg: "El usuario ya existe"})
+            } else {
+
+                await models.Persona.create({
+                    name: req.body.name,
+                    cc: req.body.cc,
+                    phoneNumber: req.body.phoneNumber,
+                    email: req.body.email,
+                    city: req.body.city,
+                    address: req.body.address,
+                    barrio: req.body.barrio,
+                    puesto: req.body.puesto,
+                    mesa: req.body.mesa,
+                    lider: req.body.lider,
+                    user: req.body.user
+                });
+                res.status(200).json({msg: "Persona guardada"});
+            }
         } catch(err) {
             res.status(500).send(err)
         }
--- routes/persona.js
+++ routes/persona.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const personaController = require('../controllers/personaController');
 
-router.get('/', personaController.getPersonas);
+router.get('/info', personaController.getPersonas);
 router.post('/send', personaController.postPersona);
 
 module.exports = router;
